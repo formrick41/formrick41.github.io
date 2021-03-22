42e67c6dffb59ff0cd1be6015a5b208623abcd4a
formrick41 <81169830+formrick41@users.noreply.github.com>
2021-03-22 07:49:53 -0600

    Create CtrlSaludo.js

---
+++ js/CtrlSaludo.js
@@ -0,0 +1,13 @@
+"use strict"; 
+var forma = document.getElementById("forma");
+function saluda() {
+        /* Se evalúa la expresión "nombre.value" que está dentro del delimitador
+         * ${ }, para posteriormente sustituirlo dentro de la cadena.
+         * El resultado se deja en la variable "respuesta".
+         * EL TEXTO DE LA CADENA EMPIEZA Y TERMINA CON EL ACENTO INVERTIDO. */
+        // @ts-ignore ignora los errores de esta línea (no encuentra a nombre)
+        const respuesta = `Saludos a ${nombre.value}.`;
+        // Muestra la respuesta en un cuadro de diálogo de alerta.
+        alert(respuesta);
+      }
+}
