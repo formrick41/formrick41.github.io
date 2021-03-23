724183a734a3994d9d8506cdad5c674b1d4ffcfc
formrick41 <81169830+formrick41@users.noreply.github.com>
2021-03-23 07:33:00 -0600

    Delete CtrlSaludo.js

--- js/CtrlSaludo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var forma = document.getElementById("forma"),
-    salida = document.getElementById("salida");
-forma.addEventListener("input", saluda, false);
-function saluda(){
-  var nombre1 = forma["nombre1"].value.trim(),
-      nombre2 = forma["nombre2"].value.trim();
-  salida.textContent = "Saludos a "+nombre1 + " y "+ nombre2;    
-}
